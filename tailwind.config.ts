f07ff0d90c3a5f8cb626ac89ee9881e60af05739
awalhossain <waqin21@gmail.com>
2024-03-11 22:18:24 +0600

    Add new directories to file watchlist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/HomePage/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
