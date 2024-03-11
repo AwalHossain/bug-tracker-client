48a060270fe538abe7d9ec765c20c89025ac3f75
awalhossain <waqin21@gmail.com>
2024-03-11 22:15:01 +0600

    Add image domains and enable React strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["clickup.com", "i.ibb.co"],
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
