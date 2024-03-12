8c1ef94521eb95239decba7843fb6632a765c0f2
awalhossain <waqin21@gmail.com>
2024-03-13 04:19:43 +0600

    Refactor authApi.ts to log data sent to the server and update userRegister endpoint URL

--- src/redux/api/auth/authApi.ts
+++ src/redux/api/auth/authApi.ts
@@ -5,18 +5,20 @@ const AUTH_URL = "/auth";
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     userLogin: build.mutation({
-      query: (data) => ({
-        url: `${AUTH_URL}/login`,
-        method: "POST",
-        body: data,
-      }),
+      query: (data) => {
+        console.log("Data sent to the server: ", data);
+        return {
+          url: `${AUTH_URL}/login`,
+          method: "POST",
+          data,
+        };
+      },
     }),
-
     userRegiser: build.mutation({
       query: (data) => ({
-        url: `${AUTH_URL}/register`,
+        url: `${AUTH_URL}/create`,
         method: "POST",
-        body: data,
+        data,
       }),
     }),
   }),
