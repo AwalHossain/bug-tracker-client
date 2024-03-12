a32381b73b113ed49dd121ea6273c21f904699db
awalhossain <waqin21@gmail.com>
2024-03-12 12:08:05 +0600

    Add AuthProvider component

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,9 @@
+// import { IAuthContext } from "@/types/Auth";
+// import { createContext } from "react";
+
+// export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+
+// function AuthProvider({ children }: { children: React.ReactNode }) {
+
+//   return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+// }
