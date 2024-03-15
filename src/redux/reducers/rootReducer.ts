5a7fbc8ade1fcbf120384260fe04c62ef8fd0606
awalhossain <waqin21@gmail.com>
2024-03-15 22:55:51 +0600

    Add authSlice to rootReducer

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,5 +1,7 @@
+import authSlice from "../api/auth/authSlice";
 import { baseApi } from "../api/baseApi";
 
 export const reducer = {
   [baseApi.reducerPath]: baseApi.reducer,
+  auth: authSlice,
 };
