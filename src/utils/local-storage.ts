4ef4727b7a6eb2c9ac42d842b6dd7d6ebd000838
awalhossain <waqin21@gmail.com>
2024-03-15 22:56:04 +0600

    Add new functions for local storage and access token retrieval

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,3 +1,6 @@
+import axiosInstance from "@/helpers/axios/axiosInstance";
+import { getBaseUrl } from "@/helpers/config/envConfig";
+
 export const setToLocalStorage = (key: string, token: string) => {
   if (!key || typeof window === "undefined") {
     return "";
@@ -9,5 +12,16 @@ export const getFromLocalStorage = (key: string) => {
   if (!key || typeof window === "undefined") {
     return "";
   }
-  return localStorage.getItem(JSON.parse(key));
+  return localStorage.getItem(key);
+};
+
+export const getNewAccesstoken = async () => {
+  return await axiosInstance({
+    url: `${getBaseUrl()}/auth/refresh-token`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
 };
