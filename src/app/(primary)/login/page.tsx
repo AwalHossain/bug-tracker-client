4019ff702ea682c67fc83dfbf218591a58408000
awalhossain <waqin21@gmail.com>
2024-03-24 21:54:11 +0600

    Refactor login page component and add invitation functionality

--- src/app/(primary)/login/page.tsx
+++ src/app/(primary)/login/page.tsx
@@ -1,30 +1,35 @@
 "use client";
+import { FormInput } from "@/components/auth/FormInput";
+import { UserForm } from "@/components/auth/UserForm";
 import CircleLoader from "@/components/custom/CircleLoader";
+import { useInvitation } from "@/hooks/useInvitation";
 import { useUserLoginMutation } from "@/redux/api/auth/authApi";
 import { storeUserInfo } from "@/services/auth.service";
 import { IUser } from "@/types/Auth";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import logo from "../../../assets/images/grabbug-logo.png";
+import logo from "../../../assets/images/bug-logo.png";
 
 export default function Login() {
-  const { register, handleSubmit } = useForm<IUser>();
+  const { register, handleSubmit, setValue } = useForm<IUser>();
   const [userLogin, { isLoading }] = useUserLoginMutation();
+  const { workspaceId, localEmail, token } = useInvitation();
   const router = useRouter();
 
   const handelLogin = async (data: IUser): Promise<void> => {
     try {
-      if (data.password!.length < 6) {
-        toast.error("Password must be at least 6 characters !!!");
-        return;
+      if (workspaceId && localEmail && token) {
+        data.workspaceId = workspaceId;
+        data.token = token;
       }
       console.log("data from the login page", data);
 
-      const res = await userLogin(data).unwrap();
-      console.log("res from the login page", res);
+      // const res = await userLogin(data).unwrap();
+      console.log("res from the login pagewo", res);
 
       if (res?.data?.accessToken) {
         toast.success("Login successful");
@@ -32,7 +37,7 @@ export default function Login() {
       storeUserInfo({ accessToken: res?.data?.accessToken });
 
       // redirect to the dashboard
-      router.replace("/dashboard/tickets");
+      router.replace("/workspace");
     } catch (error: any) {
       console.log("error from the login page", error?.data as any);
 
@@ -40,24 +45,17 @@ export default function Login() {
     }
   };
 
-  // const handle = async (e) => {
-  //     if (data.password!.length < 6) {
-  //         return cogoToast.error("Password must be at least 6 characters !!!");
-  //     }
-  //     await login(data);
-  //     e.preventDefault();
-  //     console.log("sometiung");
-  // };
+  useEffect(() => {
+    if (localEmail) {
+      console.log("localEmail", localEmail);
+      setValue("email", localEmail);
+    }
+  }, [localEmail, setValue]);
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100 py-10">
-      <form
-        onSubmit={handleSubmit(handelLogin)}
-        // onSubmit={(e) => handle(e)}
-        className="mx-6 flex w-full flex-col space-y-6 rounded-lg border bg-white px-7 py-10 shadow-lg lg:w-2/5"
-      >
-        {/* logo */}
-        <div className="mx-auto w-44">
+      <UserForm register={register} onSubmit={handleSubmit(handelLogin)}>
+        <div className="mx-auto w-20">
           <Image src={logo} alt="Logo" />
         </div>
         {isLoading && (
@@ -65,33 +63,28 @@ export default function Login() {
             <CircleLoader />
           </div>
         )}
-        <input
-          className="rounded-lg border-gray-300  py-4 text-sm shadow transition hover:shadow-lg"
+        <FormInput
+          register={register}
           type="email"
-          required
+          name="email"
           placeholder="Email"
-          {...register("email")}
         />
-        <input
-          className="rounded-lg border-gray-300 py-4  text-sm shadow transition hover:shadow-lg"
+        <FormInput
+          register={register}
           type="password"
-          required
+          name="password"
           placeholder="Password"
-          {...register("password")}
         />
-
         <button className="primary-btn" type="submit">
-          Sign in
+          Login
         </button>
-
-        {/* already registered */}
         <p className="text-center text-sm font-semibold">
-          {`Don't have an account?`}
+          {` Don't have an account ?`}
           <Link href="/register" className="text-blue-500">
             Register
           </Link>
         </p>
-      </form>
+      </UserForm>
     </div>
   );
 }
