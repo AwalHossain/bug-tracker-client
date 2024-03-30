fec3e1622edf3980bbf119433f8b6ba5fa8aa514
awalhossain <waqin21@gmail.com>
2024-03-31 00:00:56 +0600

    Refactor RegisterForm component to remove unnecessary code

--- src/components/forms/register-form.tsx
+++ src/components/forms/register-form.tsx
@@ -16,7 +16,7 @@ import { IUser } from "@/types/Auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Spinner } from "../common/Spinner";
@@ -41,16 +41,10 @@ export default function RegisterForm() {
   const [userRegiser, { isLoading }] = useUserRegiserMutation();
 
   const { workspaceId, localEmail, token } = useInvitation();
-  const defaultValues = useMemo(
-    () => ({
-      email: "demo@gmail.com",
-    }),
-    [],
-  );
   const form = useForm<UserFormValue>({
     resolver: zodResolver(formSchema),
-    defaultValues,
   });
+  const { setValue } = form;
 
   const onSubmit = async (data: IUser) => {
     // signIn("credentials", {
@@ -86,9 +80,12 @@ export default function RegisterForm() {
   useEffect(() => {
     if (localEmail) {
       console.log("localEmail", localEmail);
-      defaultValues.email = localEmail;
+      setValue("email", localEmail, {
+        shouldDirty: true,
+        shouldValidate: true,
+      });
     }
-  }, [localEmail, defaultValues]);
+  }, [localEmail, setValue]);
 
   return (
     <>
