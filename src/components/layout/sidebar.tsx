70f9ccd8e06eed76e9b0dad90e081ef9165feae3
awalhossain <waqin21@gmail.com>
2024-03-31 00:01:51 +0600

    Add responsive behavior and control button to sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,21 +1,64 @@
+import control from "@/assets/images/control.png";
 import { DashboardNav } from "@/components/dashboard-nav";
 import { navItems } from "@/constants/data";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [open, setOpen] = useState(true);
+  useEffect(() => {
+    const handleResize = () => {
+      const isMediumScreen = window.innerWidth < 1024;
+      setIsSmallScreen(isMediumScreen);
+      if (isMediumScreen) {
+        setOpen(false);
+      } else {
+        setOpen(true);
+      }
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <nav
-      className={cn(`relative hidden h-screen border-r pt-16 lg:block w-72`)}
+      className={cn(
+        `${open ? "w-[234px]" : "w-20 "} relative h-screen  border-r pt-16 
+        duration-300 text-[#2a2e34] border-gray-400 bg-[#F7F8F9] box-border
+        dark:bg-[#2A2E34] dark:text-[#F7F8F9]
+        `,
+      )}
     >
       <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
+        <div className=" py-2">
           <div className="space-y-1">
-            <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
+            {/* <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
               Overview
-            </h2>
-            <DashboardNav items={navItems} />
+            </h2> */}
+            <DashboardNav items={navItems} open={open} />
           </div>
         </div>
+      </div>
+      <div className="flex">
+        <Image
+          src={control}
+          alt="control"
+          className={`absolute cursor-pointer -right-3 top-14 w-8 border-2 border-white rounded-full duration-300 ${
+            !open && "rotate-180"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
+        {/* <p className="font-semibold uppercase text-[#b3b8d4]">Issue Tracker</p> */}
       </div>
     </nav>
   );
