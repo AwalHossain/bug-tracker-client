46a06a450965dc5834d0c4f71b51f6c39fbb585a
awalhossain <waqin21@gmail.com>
2024-04-05 14:18:30 +0600

    Update theme toggle button styling

--- src/components/layout/ThemeToggle/theme-toggle.tsx
+++ src/components/layout/ThemeToggle/theme-toggle.tsx
@@ -15,9 +15,12 @@ export default function ThemeToggle({}: CompProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          className="text-white hover:bg-[#3e4066] h-[1.90rem] p-1 transition-colors duration-200 ease-in-out rounded-md cursor-pointer bg-current/50"
+          size="icon"
+        >
+          <SunIcon className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
