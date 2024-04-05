367f9d5ae544f55237f4dedf3aa97418af2665ce
awalhossain <waqin21@gmail.com>
2024-04-05 14:20:10 +0600

    Add workspace tag type to tagTypesList

--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -1,5 +1,6 @@
 export enum tagTypes {
   user = "user",
+  workspace = "workspace",
 }
 
-export const tagTypesList = [tagTypes.user];
+export const tagTypesList = [tagTypes.user, tagTypes.workspace];
