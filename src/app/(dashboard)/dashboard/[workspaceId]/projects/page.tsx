e183c37b7c8c805c7ef988ebd69f02d33bf7fda5
awalhossain <waqin21@gmail.com>
2024-04-05 14:17:38 +0600

    Refactor Projects page component and add IssueBoard component

--- src/app/(dashboard)/dashboard/[workspaceId]/projects/page.tsx
+++ src/app/(dashboard)/dashboard/[workspaceId]/projects/page.tsx
@@ -1,54 +1,16 @@
 "use client";
 
-import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
-import { ModuleRegistry } from "@ag-grid-community/core";
-import { AgGridReact } from "@ag-grid-community/react";
-import "@ag-grid-community/styles/ag-grid.css";
-import "@ag-grid-community/styles/ag-theme-quartz.css";
-import { useCallback, useMemo, useState } from "react";
-ModuleRegistry.registerModules([ClientSideRowModelModule]);
+import IssueBoard from "@/components/tables/issue-tables/issue-board";
+import { useRouter } from "next/navigation";
 
 export default function Projects() {
-  const containerStyle = useMemo(
-    () => ({ width: "100%", height: "100%", padding: "12px" }),
-    [],
-  );
-  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
-  const [rowData, setRowData] = useState();
-  const [columnDefs, setColumnDefs] = useState([
-    { field: "athlete", rowDrag: true },
-    { field: "country" },
-    { field: "year", width: 100 },
-    { field: "date" },
-    { field: "sport" },
-    { field: "gold" },
-    { field: "silver" },
-    { field: "bronze" },
-  ]);
-  const defaultColDef = useMemo(() => {
-    return {
-      width: 170,
-      filter: true,
-    };
-  }, []);
-
-  const onGridReady = useCallback((params) => {
-    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
-      .then((resp) => resp.json())
-      .then((data) => setRowData(data.slice(0, 10)));
-  }, []);
-
+  const router = useRouter();
   return (
-    <div style={containerStyle}>
-      <div style={gridStyle} className={"ag-theme-quartz-dark"}>
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          rowDragManaged={true}
-          onGridReady={onGridReady}
-        />
+    <>
+      <div className=" p-4 md:p-8 pt-6">
+        <p>This is issue Board</p>
+        <IssueBoard />
       </div>
-    </div>
+    </>
   );
 }
