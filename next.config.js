12e8e6031cef9f588bb43b4d7907a67b86f686fd
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-04-10 02:06:41 +0000

    Add remotePatterns for utfs.io and cdn.pixabay.com in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
       {
         protocol: "https",
         hostname: "clickup.com",
