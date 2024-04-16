f889a0a912a460418f176a605d232f328ecd664a
awalhossain <waqin21@gmail.com>
2024-04-16 21:43:01 +0600

    Refactor DashboardLayout component: Remove unused imports and loading spinner

--- src/app/(dashboard)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(dashboard)/dashboard/[workspaceId]/layout.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { Spinner } from "@/components/common/Spinner";
+import AuthGuard from "@/components/layout/AuthGuard";
 import Header from "@/components/layout/header";
 import Sidebar from "@/components/layout/sidebar";
-import useAuth from "@/hooks/useAuth";
-import { useGetOneWorkspaceQuery } from "@/redux/api/workspace/workspaceApi";
 import type { Metadata } from "next";
-import { useParams } from "next/navigation";
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Dashboard",
   description: "bug tracker dashboard ",
 };
@@ -18,29 +13,15 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { isAuthenticated, isLoading } = useAuth();
-
-  // get workspace id from url
-  const { workspaceId } = useParams();
-
-  const { isLoading: workSpaceLoading } = useGetOneWorkspaceQuery(
-    workspaceId as string,
-    {
-      refetchOnMountOrArgChange: true,
-    },
-  );
-
-  if (isLoading || workSpaceLoading) return <Spinner className="middle" />;
-
   return (
-    isAuthenticated && (
-      <>
+    <>
+      <AuthGuard>
         <Header />
         <div className="flex h-screen overflow-hidden">
           <Sidebar />
           <main className="w-full pt-16">{children}</main>
         </div>
-      </>
-    )
+      </AuthGuard>
+    </>
   );
 }
