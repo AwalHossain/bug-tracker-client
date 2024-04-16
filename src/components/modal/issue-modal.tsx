17bf78feea61ba45980de7651fef95ff903c17cf
awalhossain <waqin21@gmail.com>
2024-04-16 21:44:08 +0600

    Refactor: Update imports and remove unused code

--- src/components/modal/issue-modal.tsx
+++ src/components/modal/issue-modal.tsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogOverlay,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogOverlay, DialogTrigger } from "@/components/ui/dialog";
 import CreateIssue from "./create-issue";
 
 export default function IssueModal() {
@@ -18,32 +9,7 @@ export default function IssueModal() {
       <DialogTrigger asChild>
         <Button variant="outline">Edit Profile</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[625px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you are done.
-          </DialogDescription>
-        </DialogHeader>
-        {/* <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div> */}
-        <CreateIssue />
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
+      <CreateIssue />
       <DialogOverlay />
     </Dialog>
   );
