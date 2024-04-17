e053aa8ac8e08ef70ced4134c575347e1c510064
awalhossain <waqin21@gmail.com>
2024-04-17 17:52:48 +0600

    Refactor AssigneUser component: Update AvatarImage import and add name initials to AvatarFallback

--- src/components/issue/AssigneUser.tsx
+++ src/components/issue/AssigneUser.tsx
@@ -1,14 +1,22 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 export default function AssigneUser({
   photoURL,
+  name,
 }: {
   photoURL: string | undefined;
+  name: string;
 }) {
+  const initials = name
+    .split(" ")
+    .map((n) => n[0] + n[1])
+    .join("");
+
   return (
-    <Avatar className="m-0 w-6 h-6">
+    <Avatar className="m-0 w-5 h-5">
       <AvatarImage className="" src={photoURL} alt="@shadcn" />
-      <AvatarFallback>CN</AvatarFallback>
+      <AvatarFallback>{initials}</AvatarFallback>
     </Avatar>
   );
 }
