6b5492ec615767bf81696d21011ef622ef7f87a6
awalhossain <waqin21@gmail.com>
2024-04-17 17:53:04 +0600

    Refactor CreateIssue component: Add taskName state and handleSubmit function

--- src/components/issue/create-issue.tsx
+++ src/components/issue/create-issue.tsx
@@ -1,49 +1,69 @@
 "use client";
 
 import { StickyNote } from "lucide-react";
+import { useParams } from "next/navigation";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { DialogContent, DialogFooter } from "../ui/dialog";
 import AssigneBox from "./AssigneBox";
 import CalendarBox from "./CalenderBox";
+import LabelBox from "./LabelBox";
 import PriorityBox from "./PriorityBox";
-import TodoBox from "./TodoBox";
 
 export default function CreateIssue() {
-  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+  const { workspaceId } = useParams();
+  const [taskName, setTaskName] = useState("");
   const [description, setDescription] = useState("");
-  const [open, setOpen] = useState(false);
+  const [openDS, setOpenDS] = useState(false);
 
-  const handleDescriptionToggle = () => {
-    setIsDescriptionOpen(!isDescriptionOpen);
-  };
+  const [assignee, setAssignee] = useState("");
+  const [date, setDate] = useState<Date>();
+  const [priority, setPriority] = useState("");
+  const [label, setLabel] = useState("");
 
-  const handleDescriptionChange = (e) => {
+  const handleDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setDescription(e.target.value);
   };
 
-  const handleOpen = () => {
-    setOpen(true);
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskName(e.target.value);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleOpen = () => {
+    setOpenDS(true);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const taskData = {
+      title: taskName,
+      description: description,
+      assignedToId: assignee,
+      deadline: date?.toISOString(),
+      priority: priority,
+      workspaceId: workspaceId,
+      label: label,
+    };
+
+    console.log(taskData, "Task Data");
   };
 
   return (
     <DialogContent className="sm:max-w-[625px]">
       <div className="mt-10 w-full">
         <input
-          className="bg-transparent border-none outline-none placeholder:text-gray-400 w-full placeholder:font-normal placeholder:text-lg"
+          type="text"
+          onChange={handleTaskChange}
+          className="bg-white border border-gray-300 rounded-md px-3 py-2 w-full text-lg placeholder-gray-500 outline-none focus:ring-2 focus:ring-blue-600"
           placeholder={"Input your task name"}
         />
-        {open ? (
-          <>
-            <textarea
-              className="w-full h-32 mt-4 p-2 border border-gray-300 rounded-md outline-none border-none resize-none placeholder:text-gray-400 placeholder:font-normal placeholder:text-lg"
-              placeholder="Enter task description"
-            />
-          </>
+        {openDS ? (
+          <textarea
+            onChange={handleDescription}
+            className="w-full h-32 mt-4 p-2 border border-gray-300 rounded-md outline-none resize-none placeholder-gray-500 text-lg focus:ring-2 focus:ring-blue-600"
+            placeholder="Enter task description"
+          />
         ) : (
           <button
             onClick={handleOpen}
@@ -57,18 +77,20 @@ export default function CreateIssue() {
         )}
       </div>
       <div className="flex items-center  p-2 gap-x-3">
-        <TodoBox />
+        {/* <TodoBox /> */}
         {/* <button className=" flex items-center justify-center gap-x-1 rounded-md bg-gray-200 px-2 py-1 hover:bg-blue-600">
         </button> */}
-        <AssigneBox />
-        <CalendarBox />
-        <PriorityBox />
-
+        <AssigneBox setAssignee={setAssignee} />
+        <CalendarBox setDate={setDate} />
+        <PriorityBox setPriority={setPriority} />
+        <LabelBox setLabel={setLabel} />
         {/* Add more buttons or elements as needed */}
       </div>
       {/* <CreateIssue /> */}
       <DialogFooter>
-        <Button type="submit">Save changes</Button>
+        <Button type="submit" onClick={handleSubmit}>
+          Save changes
+        </Button>
       </DialogFooter>
     </DialogContent>
   );
