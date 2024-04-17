852930cfb1874be678fd268bf2a3106683ae4f9b
awalhossain <waqin21@gmail.com>
2024-04-17 17:54:21 +0600

    Refactor: Add open state and handleOpenModal and handleCloseModal functions

--- src/components/modal/issue-modal.tsx
+++ src/components/modal/issue-modal.tsx
@@ -1,13 +1,31 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogOverlay, DialogTrigger } from "@/components/ui/dialog";
+import { useState } from "react";
 import CreateIssue from "../issue/create-issue";
 
 export default function IssueModal() {
+  const [open, setOpen] = useState(false);
+  const [taskName, setTaskName] = useState("");
+  const [isConfirmingClose, setIsConfirmingClose] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
   return (
-    <Dialog>
-      <DialogOverlay className=" " />
+    <Dialog
+      open={open}
+      onOpenChange={(isOpen) => !isOpen && handleCloseModal()}
+    >
+      <DialogOverlay className="" />
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button variant="outline" onClick={handleOpenModal}>
+          Edit Profile
+        </Button>
       </DialogTrigger>
       <CreateIssue />
       <DialogOverlay />
